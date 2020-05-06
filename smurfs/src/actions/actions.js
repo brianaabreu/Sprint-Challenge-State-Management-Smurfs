623779c9cf09867a50685480b7e8c9144f853d41
brianaabreu <briana.abreu01@gmail.comgit config --global user.email briana.abreu01@gmail.comgit config --global user.name brianaabreugit config --global user.email briana.abreu01@gmail.com>
2020-05-06 13:15:27 -0700

    mvp

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -4,7 +4,6 @@ export const FETCH_DATA = 'FETCH_DATA';
 export const UPDATE_SMURFS = 'UPDATE_SMURFS';
 export const SET_ERROR = 'SET_ERROR';
 export const ADD_SMURF = 'ADD_SMURF';
-export const POST_SUCCESS = 'POST_SUCCESS'
 
 export const getData = () => dispatch => {
     dispatch({ type: FETCH_DATA });
